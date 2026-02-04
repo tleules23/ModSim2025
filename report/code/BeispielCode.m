fa7ed85bb59d3adcf82eb514872648f29ebb083e
hasanguenes <guenes.hasan@outlook.com>
2026-02-04 20:48:25 +0100

    added report folder including overleaf project

---
+++ report/code/BeispielCode.m
@@ -0,0 +1,10 @@
+function [x, y] = polar2cartesian(r,phi)
+%POLAR2CARTESIAN converts polar into cartesian coordinates
+
+  if r < 0
+    disp('Error in ''polar2cartesian''! Negative values for ''r'' are not allowed!');
+  end
+
+  x = r*cos(phi);
+  y = r*sin(phi);
+end
